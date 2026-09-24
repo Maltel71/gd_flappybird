8436f46d6fcfeb178376a899970f6d6ec94356bd
Maltel71 <156253099+Maltel71@users.noreply.github.com>
2026-09-24 19:04:39 +0200

    sprites added

--- gd-flappybird/malte/scenes/malte_level_01.gd
+++ gd-flappybird/malte/scenes/malte_level_01.gd
@@ -7,7 +7,10 @@ extends Node2D
 var score: int = 0
 var high_score: int = 0
 
+var save_file_path := "user://save_game.dat"
+
 func _ready() -> void:
+	load_high_score()
 	hud.set_score(score)
 
 func _on_player_malte_game_started() -> void:
@@ -20,6 +23,8 @@ func _on_player_malte_died() -> void:
 	ground_malte.animation_player.pause()
 	get_tree().call_group("obstacles", "stop")
 	
+	
+	
 	await get_tree().create_timer(0.5).timeout
 	hud.show_game_over_screen(score, 123)
 
@@ -27,3 +32,14 @@ func _on_player_malte_died() -> void:
 func _on_player_malte_scored() -> void:
 	score += 1
 	hud.set_score(score)
+	
+func save_high_score() -> void:
+	var save_data = FileAccess.open(save_file_path, FileAccess.WRITE)
+	save_data.store_var(high_score)
+	save_data.close()
+	
+func load_high_score() -> void:
+	if FileAccess.file_exists(save_file_path):
+		var save_data = FileAccess.open(save_file_path, FileAccess.READ)
+		high_score = save_data.get_var()
+		save_data.close()
